a9cee3176cc0654fb9e834b12599a6859598e19c
FSGomez <fergo2005@gmail.com>
2025-02-03 18:14:52 -0300

    version 202502031800

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ console.log("ESTA ACTIVO JS");
 const habitaciones = [
     {
         imagen: "img/twin-4.jpg",
-        tipo: "Single",        
+        tipo: "Single",
         servicios: [" Tv de pantalla plana hasta ", "Wi-Fi gratis"],
-        clasificacion: 9.5,
+        clasificacion: 9,
         precio: 100000,
         disponibilidad: 5
     },
     {
         imagen: "img/doble-twing-06.jpg",
         tipo: "Doble twin",
-        servicios:["Tv de panta plana"],
-        clasificacion: 9.5,
+        servicios: ["Tv de panta plana"],
+        clasificacion: 9,
         precio: 200000,
         disponibilidad: 10
     },
@@ -23,15 +23,15 @@ const habitaciones = [
         imagen: "img/doble-matrim03.jpg",
         tipo: "Doble matrimonial",
         servicios: ["Tv de panta plana"],
-        clasificacion: 9.5,
+        clasificacion: 9,
         precio: 250000,
         disponibilidad: 10
     },
     {
         imagen: "img/cuadruple-05.jpg",
         tipo: "Triple Twin",
-        servicios: ["Tv de panta plana"],    
-        clasificacion: 9.5,
+        servicios: ["Tv de panta plana"],
+        clasificacion: 8,
         precio: 280000,
         disponibilidad: 8
     },
@@ -39,26 +39,35 @@ const habitaciones = [
         imagen: "img/triple-03-1.jpg",
         tipo: "Triple Mat + 1",
         servicios: ["Tv de panta plana"],
-        clasificacion: 9.5,
+        clasificacion: 9,
         precio: 320000,
         disponibilidad: 2
     },
     {
         imagen: "img/cuadruple-04.jpg",
         tipo: "Cuadruple",
-        servicios: ["Tv de panta plana"],     
-        clasificacion: 9.5,
+        servicios: ["Tv de panta plana"],
+        clasificacion: 9,
         precio: 400000,
         disponibilidad: 1
+    },
+    {
+        imagen: "img/cuadruple-04.jpg",
+        tipo: "Quinta",
+        servicios: ["Tv de panta plana", "algo mas 2", "Algo mas 3"],
+        clasificacion: 7,
+        precio: 500000,
+        disponibilidad: 0
     }
+
 ]
 
 
 
 
 
-let habitacionesHtml= "";
-for (let i = 0; i<habitaciones.length; i++) {
+let habitacionesHtml = "";
+for (let i = 0; i < habitaciones.length; i++) {
     habitacionesHtml += `
         <div class="target-habitacion">
             <div class="target-hab-img">
@@ -80,8 +89,8 @@ for (let i = 0; i<habitaciones.length; i++) {
                     <p class="h-precio">${habitaciones[i].precio}</p>
                     <p class="h-disponibles">Cant. ${habitaciones[i].disponibilidad}</p>
                     <div class="div-enlace-res">
-                        <a class="t-enlace-res" href="#">Reservar</a>
-                    </div>
+                        <input type="button" class="btn-enlace-res" id="agregar-reserva" value="RESERVAR">
+                    </div>                    
                 </div>
             </div>
         </div>    
@@ -93,5 +102,5 @@ for (let i = 0; i<habitaciones.length; i++) {
 /* tomamos el elemento div id habitaciones */
 const containerHabitaciones = document.getElementById("contenedorHabitaciones");
 /* A su contenido lo editamos agregandole lo que armamos */
- containerHabitaciones.innerHTML = habitacionesHtml;
+containerHabitaciones.innerHTML = habitacionesHtml;
 
